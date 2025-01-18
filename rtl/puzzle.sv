afcdf802bdfa907203621a711a436c575b608edb
Maciej Bielecki <zyla@prati.pl>
2025-01-18 19:01:01 +0100

    WIP

--- rtl/puzzle.sv
+++ rtl/puzzle.sv
@@ -8,48 +8,60 @@ module puzzle (
     input wire  [7:0] input_data,   // The recieved data.
 
     input  wire       output_busy,  // Module busy sending previous item.
-    output reg       output_en,    // Send the data on output_data
-    output reg [7:0] output_data   // The data to be sent
+    output logic       output_en,    // Send the data on output_data
+    output logic [7:0] output_data   // The data to be sent
 );
 
   reg [7:0] n_to_send = 0;
+  typedef enum { INITIAL, SEND, SENDING } State;
+  State state = INITIAL;
 
-  enum { INITIAL, SEND, SENDING } state = INITIAL;
+  State next_state;
+  logic [7:0] next_n_to_send;
 
   always @(posedge clk) begin
     if (rst) begin
       n_to_send <= 0;
       state <= INITIAL;
     end else begin
-      case(state)
-        INITIAL: begin
-          if (input_valid && input_data >= "0" && input_data <= "9") begin
-            state <= SEND;
-            n_to_send <= input_data - "0";
-          end
-          output_en <= 0;
-        end
-        SEND: begin
-          output_en <= 0;
-          if(!output_busy) begin
-            if(n_to_send > 0) begin
-              state <= SENDING;
-              output_data <= "A";
-              output_en <= 1;
-              n_to_send <= n_to_send - 1;
-            end else begin
-              state <= INITIAL;
-            end
-          end
-        end
-        SENDING: begin
-          output_en <= 0;
-          if(!output_busy) begin
-            state <= SEND;
-          end
-        end
-      endcase
+      state <= next_state;
+      n_to_send <= next_n_to_send;
     end
+  end
+
+  always_comb begin
+    case(state)
+      INITIAL: begin
+        if (input_valid && input_data >= "0" && input_data <= "9") begin
+          next_state = SEND;
+          next_n_to_send = input_data - "0";
+        end else begin
+          next_state = INITIAL;
+        end
+        output_en = 0;
+      end
+      SEND: begin
+        if(!output_busy) begin
+          if(n_to_send > 0) begin
+            next_state = SENDING;
+            output_data = "A";
+            output_en = 1;
+            next_n_to_send = n_to_send - 1;
+          end else begin
+            next_state = INITIAL;
+            output_en = 0;
+          end
+        end else begin
+          output_en = 0;
+        end
+      end
+      SENDING: begin
+        output_en = 0;
+        if(!output_busy) begin
+          next_state = SEND;
+        end
+      end
+    endcase
   end
 
 endmodule
